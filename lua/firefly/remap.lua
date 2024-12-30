5c96897c144cbe6e6d556fa05cc4c67ff210ac4a
X4ndras <daniel.shd.2004@gmail.com>
2024-12-30 12:20:15 +0100

    added window nav keybinds

--- lua/firefly/remap.lua
+++ lua/firefly/remap.lua
@@ -1,5 +1,15 @@
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<M-h>", "<C-w>h")
+vim.keymap.set("n", "<M-j>", "<C-w>j")
+vim.keymap.set("n", "<M-k>", "<C-w>k")
+vim.keymap.set("n", "<M-l>", "<C-w>l")
+
+vim.keymap.set("n", "<M-H>", "<C-w>H")
+vim.keymap.set("n", "<M-J>", "<C-w>J")
+vim.keymap.set("n", "<M-K>", "<C-w>K")
+vim.keymap.set("n", "<M-L>", "<C-w>L")
+
 -- fluent scrolling
 local cinnamon = require("cinnamon")
 cinnamon.setup()
@@ -24,17 +34,6 @@ vim.keymap.set({
     }, '<M-;>', '<ESC>',
     { noremap = true, silent = true
 })
-
---[[
-vim.cmd.nnoremap("<M-;>", "<ESC>")
-vim.cmd.inoremap("<M-;>", "<ESC>")
-vim.cmd.vnoremap("<M-;>", "<ESC>")
-vim.cmd.cnoremap("<M-;>", "<ESC>")
-vim.api.nvim_set_keymap('o', '<M-;>', '<ESC>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('s', '<M-;>', '<ESC>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', '<M-;>', '<ESC>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', '<M-;>', '<ESC>', { noremap = true, silent = true })
-]]--
 
 vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
 
@@ -90,5 +89,5 @@ local function swap_down()
     vim.cmd('exec ' .. ln .. ' + 1')
 end
 
-vim.keymap.set('n', '<M-k>', function() swap_up() end)
-vim.keymap.set('n', '<M-j>', function() swap_down() end)
+vim.keymap.set('n', '<C-k>', function() swap_up() end)
+vim.keymap.set('n', '<C-j>', function() swap_down() end)
