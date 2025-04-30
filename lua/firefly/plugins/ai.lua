b2842b99191bfedbf858f57b9fef60154fff0aef
X4ndras <danieldietrich.2004@gmail.com>
2025-04-30 20:22:00 +0200

    feat(ai): idk anymore

--- lua/firefly/plugins/ai.lua
+++ lua/firefly/plugins/ai.lua
@@ -112,7 +112,7 @@ end
 
 require('avante').setup({
   --provider = "openai",
-  provider = "deepseek-reasoner",
+  provider = "claude-3.7",
   --auto_suggestions_provider = "",
   vendors = {
     ["deepseek-coder"] = {
@@ -138,12 +138,19 @@ require('avante').setup({
     },
     ["claude-3.7"] = {
       __inherited_from = "copilot",
+      display_name = "copilot/claude-3.7",
       model = "claude-3.7-sonnet",
     },
     ["claude-3.5"] = {
       __inherited_from = "copilot",
+      display_name = "copilot/claude-3.5",
       model = "claude-3.5-sonnet",
-    }
+    },
+    ["gpt-o3-mini"] = {
+      __inherited_from = "copilot",
+      display_name = "copilot/gpt-o3-mini",
+      model = "gpt-o3-mini",
+    },
   },
   chat_history = {
     enabled = true,
@@ -155,7 +162,7 @@ require('avante').setup({
     auto_suggestions = false,
     auto_suggestions_respect_ignore = false,
     auto_set_highlight_group = true,
-    auto_set_keymaps = true,
+    auto_set_keymaps = true, -- NOTE Without no key maps would work from the config
     auto_apply_diff_after_generation = false,
     jump_result_buffer_on_finish = false,
     support_paste_from_clipboard = false,
