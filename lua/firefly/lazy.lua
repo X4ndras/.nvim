1320e010bb57fd81acd5ff86805875938fb0edd0
X4ndras <>
2026-01-19 20:25:10 +0100

    feat: neogit

--- lua/firefly/lazy.lua
+++ lua/firefly/lazy.lua
@@ -111,6 +111,26 @@ local plugins = {
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles" },
   },
 
+  -- NeoGit
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+      -- "nvim-mini/mini.pick",           -- optional
+      --"folke/snacks.nvim",             -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  },
+
   -- LSP icons
   { 'onsails/lspkind.nvim', event = "InsertEnter" },
 
